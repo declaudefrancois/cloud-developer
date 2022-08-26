fbefbd1c6741a1c16a82cf3d80495b3775fee2c1
Guy Francois de Claude NKOUKA <declaudefrancois@gmail.com>
2022-08-27 00:21:47 +0100

    Project-02 submission

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -37,3 +37,11 @@ export async function deleteLocalFiles(files: Array<string>) {
     fs.unlinkSync(file);
   }
 }
+
+
+// validate an url
+export const isUrlValid = (image_url: string): boolean  => {
+  const regex = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/ig;
+  
+  return regex.test(image_url);image_url
+}
